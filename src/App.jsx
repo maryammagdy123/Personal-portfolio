587098d8b2d3210cd041c587db0bf546d95ad3fe
Maryam Magdy <mayamga9@gmail.com>
2025-07-26 19:20:45 +0300

    Updated Layout import fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
 
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import './App.css'
-import LayOut from './Layout/LayOut'
+
 import About from './Components/About/About'
 import Portfolio from './Components/Portfolio/Portfolio'
 
 import Contact from './Components/Contact/Contact'
 import Home from './Components/Home/Home'
+import Layout from './Layout/Layout'
 
 
 
 
 function App() {
- let Routes=createBrowserRouter([{
+  let Routes = createBrowserRouter([{
 
-   path:"/",element:<LayOut/>,children:[
-    {index:true,element:<Home/>},
-    {path:"home",element:<Home/>},
-    {path:"about",element:<About/>},
-    {path:"portfolio",element:<Portfolio/>},
-    {path:"contact",element:<Contact/>}
-   ]
- }])
+    path: "/", element: <Layout />, children: [
+      { index: true, element: <Home /> },
+      { path: "home", element: <Home /> },
+      { path: "about", element: <About /> },
+      { path: "portfolio", element: <Portfolio /> },
+      { path: "contact", element: <Contact /> }
+    ]
+  }])
 
- return <RouterProvider router={Routes}/>
+  return <RouterProvider router={Routes} />
 }
 
 export default App
--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -3,7 +3,7 @@ import Navbar from './Navbar/Navbar'
 import { Outlet } from 'react-router'
 import Footer from './Footer/Footer'
 
-export default function LayOut() {
+export default function Layout() {
 	return (
 		<>
 			<Navbar />
